21e95e1d6efb137cb347d47f29b90d2ef22c4179
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-05 09:48:17 +0200

    :sparkles: adds header component

---
+++ src/components/landing-page/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    const name = "Caroline"; 
+</script> 
+
+
+<template>
+    <header>
+        <h1>{{ name }}</h1>
+    </header>
+</template>
+
+
+<style scoped lang="scss">
+    header {
+        height: 300px; 
+        width: 100vw;
+        background-color: greenyellow;
+        color: black; 
+    }
+</style>
+
