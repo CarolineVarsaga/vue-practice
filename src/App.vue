45d0335bd95d4b1f1003d3fd5d9841111123723a
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-07 11:46:35 +0200

    :sparkles: imports todo

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 <script setup lang="ts">
   import Header from './components/landing-page/Header.vue'; 
   import Loader from './components/Loader.vue';
+  import TodoList from './components/TodoList.vue'; 
   
 </script>
 
 <template>
   <Header />
   <Loader />
+  <TodoList /> 
 </template>
 
 <style scoped></style>
