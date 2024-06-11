a54d548b1737203617204c2eb440195326c05e4d
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-11 15:47:49 +0200

    :sparkles: adds todos props

---
+++ src/components/Todos.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import type { Todo } from "@/models/Todo"; 
+    import { defineProps } from "vue"; 
+
+    interface ITodosProps {
+        todos: Todo[];
+    }
+    defineProps<ITodosProps>(); 
+</script>
